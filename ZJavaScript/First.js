2a30163d831ad07a94ad053fafc3fc620aaeaa58
VinithaKorivi <kvineetha135@gmail.com>
2026-03-11 20:19:55 +0530

    ZJavaScript/Index.html

---
+++ ZJavaScript/First.js
@@ -0,0 +1,6 @@
+ console.log("Hello world");
+ console.log("My name is vinitha")
+ console.log("This is my first java script code")
+console.warn("Enter proper user name")
+console.error("404 page not found")
+console.info("Enter user name")
